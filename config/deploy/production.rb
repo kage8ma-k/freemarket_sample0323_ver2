429ddc663ee0e51bfef30126fa83f15fc3cdcd58
jiujianz <cocobskg@gmail.com>
2019-04-20 14:59:58 +0900

    edit unicorn settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.197.117.28', user: 'ec2-user', roles: %w{app db web}
+server '18.182.87.11', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
