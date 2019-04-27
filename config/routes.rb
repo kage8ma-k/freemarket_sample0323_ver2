189b2a2f3ebf72de058d8abfa612f9e0eecba653
hoshimon0518 <hoshino@andbiz.co.jp>
2019-04-27 14:44:22 +0900

    【WIP】setup devise modify (devise -> user)

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,9 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items
 end
+
+
+# signup/　メール、FB、Gglでの新規登録選択
+# signup/registration/　メアド等登録　（入力フォーム記載ミスなどあっても戻るだけ）
+# signup/sms_confirmation/　電話番号の入力
+# 　　signup/sms_confirmation/sms/　過去登録している電話番号などエラーが起きた場合のURL
