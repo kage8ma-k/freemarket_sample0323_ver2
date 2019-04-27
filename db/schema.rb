df655afb05f7c3cacfce19ade86a339bcefaa399
hoshimon0518 <hoshino@andbiz.co.jp>
2019-04-27 14:46:19 +0900

    【WIP】setup devise Make ***UserProfile Model***

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190413045244) do
+ActiveRecord::Schema.define(version: 20190427050026) do
+
+  create_table "user_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "profile"
+    t.string   "profile_image"
+    t.string   "lastname",                  null: false
+    t.string   "firstname",                 null: false
+    t.string   "lastname_kana",             null: false
+    t.string   "firstname_kana",            null: false
+    t.integer  "birth_year"
+    t.integer  "birth_month"
+    t.integer  "birth_day"
+    t.integer  "postal_code",               null: false
+    t.integer  "prefecture",                null: false
+    t.string   "city",                      null: false
+    t.string   "block_number"
+    t.string   "building_name"
+    t.integer  "phone_number"
+    t.integer  "the_number_of_exhabitions"
+    t.integer  "points"
+    t.integer  "user_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",               default: "", null: false
@@ -31,4 +55,5 @@ ActiveRecord::Schema.define(version: 20190413045244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "user_profiles", "users"
 end
