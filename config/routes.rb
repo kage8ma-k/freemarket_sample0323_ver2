2174f613bffb0576747bb4774d1cd64bfb572eb2
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-05-11 11:58:22 +0900

    view.edit
    個人の実装が他部分に影響してしまっていた部分があったため。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items,only: [:index, :edit, :show]
+  resources :items,only: [:index, :edit, :new, :show]
   resources :creditcards, only: [:index, :edit]
   resources :buy
   resources :users,only: [] do
