c317c7c7da3b6d7e60a14c75af79f6d39c4ea741
AKIRONAKAHIGASHI <akizo.rain@icloud.com>
2019-05-13 22:57:49 +0900

    creditcards_buy_payjp
    payjpを利用したクレジットカードでの購入

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :items,only: [:index, :edit, :new, :show]
-  resources :creditcards, only: [:index, :edit]
-  resources :buy
-  resources :users,only: [] do
+  resources :creditcards, only: [:index, :new, :show] do
+  end
+  resources :buy do
+    collection do
+      post 'purchase'
+    end
+  end
+  resources :users, only: [:index] do
     get 'personalinfomation'
     get 'logout'
   end
