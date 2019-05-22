f28bdb32004c72d34f55292ff87bb0cf5ddb8f40
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-05-22 15:09:39 +0900

    Profit.js.create
    売上-販売手数料（10%）を引いた販売利益を表示

---
+++ app/assets/javascripts/profit.js
@@ -0,0 +1,16 @@
+$(function() {
+  $(".input-price").on("keyup", function() {
+    var input_price = $(".input-price").val();
+    var fee = Math.ceil(input_price*0.9)
+    //販売手数料= 売上の10%(0.9)。Math.ceil小数点以下切り上げのJS関数
+    var comission = input_price - fee
+    if ( 300 <= input_price && input_price <= 9999999){
+      document.getElementById('fee').innerHTML = "¥"+ Number(comission).toLocaleString();
+      document.getElementById('profit').innerHTML = "¥"+ Number(fee).toLocaleString();
+    }
+    else{
+      document.getElementById('comission').innerHTML = "";
+      document.getElementById('profit').innerHTML = "";
+    }
+  });
+});
