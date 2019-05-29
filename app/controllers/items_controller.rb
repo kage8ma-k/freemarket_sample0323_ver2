65ec4be63f52176f0859a65110ef4f2ba5e782f2
kage8ma-k <kaori@taka.name>
2019-05-29 13:40:27 +0900

    edit_account-header

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
 
   def index
     @items = Item.includes(:user).limit(4).order("created_at DESC")
