476dca1d4fa0a898a1c5dcbe2d55fa51a097d6c8
jiujianz <cocobskg@gmail.com>
2019-06-05 17:51:20 +0900

    basic認証の再設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == "admin" && password == "2222"
   end
 end
 end
