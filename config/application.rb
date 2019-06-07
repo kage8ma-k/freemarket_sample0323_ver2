b31cdcd3237310a7a5a1280d2263ed638df19ca3
chiezoo1991 <chienakayama2012eb@gmail.com>
2019-06-07 22:05:01 +0900

    Validation.add
    出品できないものを追加してみた
    （エラーメッセージはまだ出ない）

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module FreemarketSample0323
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.autoload_paths += %W(#{config.root}/app/validators)
   end
 end
